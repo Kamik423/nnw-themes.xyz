1d7d5b97ff1c31f8262d59d892583f3f768a5632
Hans <contact.kamik423@gmail.com>
2022-02-11 14:30:29 +0100

    moved images to subfolder

--- Sources/ThemeSite/Theme+Fountain.swift
+++ Sources/ThemeSite/Theme+Fountain.swift
@@ -244,10 +244,10 @@ private struct ItemList<Site: Website>: Component {
 func makeInnerItem<Site: Website>(for item: Item<Site>, in site: Site) -> Component {
     return Article {
         Span {
-            (Link(url: (item as? Item<ThemeSite>)?.metadata.link ?? "") { Image(url: "/theme-icon.png", description: "Visit Theme") }).style("display: \((item as? Item<ThemeSite>)?.metadata.link == nil ? "none" : "block")")
-            (Link(url: creatorUrl(for: (item as? Item<ThemeSite>)?.metadata.creator ?? "") ?? "") { Image(url: "/creator-icon.png", description: "Visit Creator") }).style("display: \(creatorUrl(for: (item as? Item<ThemeSite>)?.metadata.creator ?? "") == nil ? "none" : "block")")
-            Link(url: (item as? Item<ThemeSite>)?.metadata.ziplink ?? "") { Image(url: "/download-icon.png", description: "Download Zip") }
-            (Link(url: (item as? Item<ThemeSite>)?.metadata.themelink ?? "") { Image(url: "/add-icon.png", description: "Install Theme to NetNewsWire") }).style("display: \(item.is_default_theme ? "none" : "block")")
+            (Link(url: (item as? Item<ThemeSite>)?.metadata.link ?? "") { Image(url: "/website-resources/theme-icon.png", description: "Visit Theme") }).style("display: \((item as? Item<ThemeSite>)?.metadata.link == nil ? "none" : "block")")
+            (Link(url: creatorUrl(for: (item as? Item<ThemeSite>)?.metadata.creator ?? "") ?? "") { Image(url: "/website-resources/creator-icon.png", description: "Visit Creator") }).style("display: \(creatorUrl(for: (item as? Item<ThemeSite>)?.metadata.creator ?? "") == nil ? "none" : "block")")
+            Link(url: (item as? Item<ThemeSite>)?.metadata.ziplink ?? "") { Image(url: "/website-resources/download-icon.png", description: "Download Zip") }
+            (Link(url: (item as? Item<ThemeSite>)?.metadata.themelink ?? "") { Image(url: "/website-resources/add-icon.png", description: "Install Theme to NetNewsWire") }).style("display: \(item.is_default_theme ? "none" : "block")")
         }.class("theme-link-buttons")
         Link(url: "/themes/\(item.title)") { H1(item.title) }
         ItemTagList(item: item, site: site)
